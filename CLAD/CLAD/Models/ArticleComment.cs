8a34a2d23b018df545d44805c6c29881671f7226
Omar Ruder <omarruder@live.nl>
2019-03-18 15:52:28 +0100

    identityuser eruit gehaald

--- CLAD/CLAD/Models/ArticleComment.cs
+++ CLAD/CLAD/Models/ArticleComment.cs
@@ -7,5 +7,10 @@ namespace CLAD.Models
 {
     public class ArticleComment
     {
+        public int ArticleId { get; set; }
+        public int AuthorId { get; set; }
+        public string Content { get; set; }
+        public int Id { get; set; }
+        public DateTime PublicationDate { get; set; }
     }
 }
