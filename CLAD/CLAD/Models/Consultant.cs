77d5febfba49333c05da0ce182a303f20ffc1143
Omar Ruder <omarruder@live.nl>
2019-03-20 13:44:43 +0100

    Consultantpage, ImgName in model, undefined.jpg toegevoegd

--- CLAD/CLAD/Models/Consultant.cs
+++ CLAD/CLAD/Models/Consultant.cs
@@ -10,5 +10,6 @@ namespace CLAD.Models
         public int Id { get; set; }
         public string Description { get; set; }
         public string DisplayName { get; set; }
+        public string ImgName { get; set; }
     }
 }
