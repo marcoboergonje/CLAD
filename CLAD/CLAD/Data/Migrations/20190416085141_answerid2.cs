6f744930f1a6759e70740833a3bef5f9a62d8aa9
Omar Ruder <omarruder@live.nl>
2019-04-16 11:28:18 +0200

    merge

---
+++ CLAD/CLAD/Data/Migrations/20190416085141_answerid2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CLAD.Data.Migrations
+{
+    public partial class answerid2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AnswerId",
+                table: "Question",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnswerId",
+                table: "Question");
+        }
+    }
+}
